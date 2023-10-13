0f56d0ad20adbfa65d2ed2f93c05a67ef7b1288b
Diogo Costa <diogoandreveigacosta@gmail.com>
2023-10-13 13:23:03 +0100

    feat(core): add cpu standby support to bao hypervisor
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- src/arch/riscv/vm.c
+++ src/arch/riscv/vm.c
@@ -87,7 +87,7 @@ void vcpu_arch_run(struct vcpu *vcpu){
     if(vcpu->arch.sbi_ctx.state == STARTED){
         vcpu_arch_entry();
     } else {
-        cpu_idle();
+        cpu_standby();
     }    
 
 }
